cda986055bc696baab209fd48196f3fa5d174c54
altiparmakzeynep <zzeynepaltiparmak@gmail.com>
2020-08-18 17:37:34 +0300

    logout

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import {Text,View,StyleSheet} from 'react-native';
+import {Text,View,StyleSheet,Button} from 'react-native';
 import firebase from 'firebase';
 import Banner from './src/components/Banner';
 import LoginForm from './src/components/LoginForm';
+import {Spinner} from './src/components/common';
 
 
 class App extends Component{
+  state = {
+    loggedIn:null
+  }
   componentDidMount(){
     firebase.initializeApp({
       apiKey: "AIzaSyCp13ayechWPFPVGFNWJHRIPKAMIQSDbYU",
@@ -17,12 +21,40 @@ class App extends Component{
       appId: "1:477252144888:web:6c7cbd5a52ff700bc7f50d",
       measurementId: "G-7HEH2KC44Y"
     });
+
+    firebase.auth().onAuthStateChanged((user)=> {
+      const loggedIn = user ? true : false;
+
+      this.setState({
+        loggedIn
+      })
+    })
   }
+  renderContent(){
+    const{loggedIn} = this.state;
+
+    switch(loggedIn){
+      case true:
+        return(
+          <Button onPress={()=> firebase.auth().signOut()}
+                  title="Logout"
+                  color="red"/>
+        )
+        case false:
+          return(
+            <LoginForm></LoginForm>
+          )
+          default:
+            return(
+              <Spinner></Spinner>
+            )
+    }
+   }
   render(){
     return(
       <View style={styles.appContainer}>
         <Banner text="Authentication"></Banner>
-        <LoginForm></LoginForm>
+        {this.renderContent()}
       </View>
 
     )
